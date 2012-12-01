51519b1a301db815f55ff2d9788f3c5d518a3719
Welby McRoberts <welby@whmcr.com>
2012-12-01 18:37:59 +0000

    Using config example for get_config

--- swift/bin/swift.py
+++ swift/bin/swift.py
@@ -54,6 +54,55 @@ SCHEME = """<scheme>
 def do_scheme():
     print SCHEME
 
+def get_config():
+    config = {}
+
+    try:
+        # read everything from stdin
+        config_str = sys.stdin.read()
+
+        # parse the config XML
+        doc = xml.dom.minidom.parseString(config_str)
+        root = doc.documentElement
+        conf_node = root.getElementsByTagName("configuration")[0]
+        if conf_node:
+            logging.debug("XML: found configuration")
+            stanza = conf_node.getElementsByTagName("stanza")[0]
+            if stanza:
+                stanza_name = stanza.getAttribute("name")
+                if stanza_name:
+                    logging.debug("XML: found stanza " + stanza_name)
+                    config["name"] = stanza_name
+
+                    params = stanza.getElementsByTagName("param")
+                    for param in params:
+                        param_name = param.getAttribute("name")
+                        logging.debug("XML: found param '%s'" % param_name)
+                        if param_name and param.firstChild and \
+                           param.firstChild.nodeType == param.firstChild.TEXT_NODE:
+                            data = param.firstChild.data
+                            config[param_name] = data
+                            logging.debug("XML: '%s' -> '%s'" % (param_name, data))
+
+        checkpnt_node = root.getElementsByTagName("checkpoint_dir")[0]
+        if checkpnt_node and checkpnt_node.firstChild and \
+           checkpnt_node.firstChild.nodeType == checkpnt_node.firstChild.TEXT_NODE:
+            config["checkpoint_dir"] = checkpnt_node.firstChild.data
+
+        if not config:
+            raise Exception, "Invalid configuration received from Splunk."
+
+        # just some validation: make sure these keys are present (required)
+        validate_conf(config, "name")
+        validate_conf(config, "key_id")
+        validate_conf(config, "secret_key")
+        validate_conf(config, "checkpoint_dir")
+    except Exception, e:
+        raise Exception, "Error getting Splunk configuration via STDIN: %s" % str(e)
+
+    return config
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "--scheme":
